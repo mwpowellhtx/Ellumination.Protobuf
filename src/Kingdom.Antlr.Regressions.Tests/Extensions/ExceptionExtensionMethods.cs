810794c41f7c92ec8348d0d6f8b1950dc9997b58
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-19 17:32:09 -0400

    added exception extension methods

---
+++ src/Kingdom.Antlr.Regressions.Tests/Extensions/ExceptionExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kingdom.Antlr.Regression
+{
+    using Xunit;
+
+    internal static class ExceptionExtensionMethods
+    {
+        /// <summary>
+        /// Verifies the <typeparamref name="T"/> <paramref name="exception"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exception"></param>
+        /// <param name="verify"></param>
+        /// <returns></returns>
+        public static T VerifyException<T>(this T exception, Action<T> verify = null)
+            where T : Exception
+        {
+            Assert.NotNull(exception);
+            verify?.Invoke(exception);
+            return exception;
+        }
+    }
+}
