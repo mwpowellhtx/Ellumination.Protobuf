d55e7a088206747a2808eee4c38d28fb20cdd89b
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-24 16:38:02 -0400

    added syntax statement interface

---
+++ src/Kingdom.Protobuf.Descriptors/Interfaces/ISyntaxStatement.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc />
+    public interface ISyntaxStatement
+        : IHasParent<ProtoDescriptor>
+    {
+    }
+}
