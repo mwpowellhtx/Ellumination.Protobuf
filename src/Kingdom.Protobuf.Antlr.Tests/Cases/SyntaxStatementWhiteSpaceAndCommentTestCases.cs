126a302e017007d7c80f8fa9c25daaf227382bac
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-24 16:44:50 -0400

    added syntax statement white space and comment test cases

---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/SyntaxStatementWhiteSpaceAndCommentTestCases.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Collections;
+    using static Domain;
+
+    internal class SyntaxStatementWhiteSpaceAndCommentTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        private static IEnumerable<object[]> PrivateCases
+            => _privateCases ?? (_privateCases
+                   = AllWhiteSpaceAndCommentOptions.Select(x => GetRange(x).ToArray()).ToArray()
+               );
+
+        protected override IEnumerable<object[]> Cases => PrivateCases;
+    }
+}
