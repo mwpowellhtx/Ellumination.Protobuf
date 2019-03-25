aabedb0597da3081f07b39e50fc7e992f3f5a3ca
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-25 13:12:55 -0400

    added white space variations of the test cases

---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/ExtendFromZeroGroupsWithWhiteSpaceTestCases.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Collections;
+    using static Domain;
+    using static Identification;
+
+    /// <summary>
+    /// Test Cases based on <see cref="ExtendFromZeroGroupsTestCases"/>. We do not
+    /// need a full set of base cases, just one or a handful. We will mix that with
+    /// the <see cref="AllWhiteSpaceAndCommentOptions"/>.
+    /// </summary>
+    internal class ExtendFromZeroGroupsWithWhiteSpaceTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        private static IEnumerable<object[]> GetBaseCases()
+        {
+            yield return GetRange<object>(
+                ElementTypes.First() // MessageType
+                , LabelKinds.First() // Label
+                , GetGroupNames(GetRange(1, 3)).ToArray() // GroupName
+                , FieldNumber
+            ).ToArray();
+        }
+
+        protected override IEnumerable<object[]> Cases
+            => _privateCases ?? (_privateCases
+                   = InsertDimension(GetBaseCases(), AllWhiteSpaceAndCommentOptions).ToArray()
+               );
+    }
+}
---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/ExtendSingleFieldFieldsWithSingleOptionWithWhiteSpaceTestCases.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Collections;
+    using static Domain;
+    using static Identification;
+
+    /// <summary>
+    /// Test Cases based on <see cref="ExtendSingleFieldFieldsWithSingleOptionTestCases"/>.
+    /// We do not need a full set of base cases, just one or a handful. We will mix that with
+    /// the <see cref="AllWhiteSpaceAndCommentOptions"/>.
+    /// </summary>
+    internal class ExtendSingleFieldFieldsWithSingleOptionWithWhiteSpaceTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        private static IEnumerable<object[]> GetBaseCases()
+        {
+            yield return GetRange<object>(
+                ElementTypes.First()
+                , GetFieldName(3)
+                , GetOptionIdentifierPath(3, 3, 3, 3, true)
+            ).ToArray();
+        }
+
+        protected override IEnumerable<object[]> Cases
+            => _privateCases ?? (_privateCases
+                   = InsertDimension(GetBaseCases(), AllWhiteSpaceAndCommentOptions).ToArray()
+               );
+    }
+}
