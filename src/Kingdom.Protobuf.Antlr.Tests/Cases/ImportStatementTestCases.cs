5103f633cb3d4377c577597ea6096799367c7a97
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-24 20:58:31 -0400

    added simple import statement test cases extended these test cases to include white space comprehension

---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/ImportStatementTestCases.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static ImportModifierKind;
+
+    internal class ImportStatementTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+
+        private static IEnumerable<object[]> PrivateCases
+        {
+            get
+            {
+                IEnumerable<object[]> GetAll()
+                {
+                    yield return Collections.GetRange<object>((ImportModifierKind?) null).ToArray();
+                    yield return Collections.GetRange<object>((ImportModifierKind?) Public).ToArray();
+                    yield return Collections.GetRange<object>((ImportModifierKind?) Weak).ToArray();
+                }
+
+                return _privateCases ?? (_privateCases = GetAll().ToArray());
+            }
+        }
+
+        protected override IEnumerable<object[]> Cases => PrivateCases;
+    }
+}
