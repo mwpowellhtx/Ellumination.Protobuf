bfb98a498b1102267ddccef3624ce3d0b64316c4
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-25 16:55:36 -0400

    added white space test case variations

---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/BasicEnumStatementWithWhiteSpaceTestCases.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Collections;
+    using static Domain;
+    using static Identification;
+
+    internal class BasicEnumStatementWithWhiteSpaceTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        private static IEnumerable<object[]> GetBaseCases()
+        {
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            foreach (var enumName in GetIdents(GetRange(1, 3)))
+            {
+                yield return GetRange<object>(enumName).ToArray();
+            }
+        }
+
+        protected override IEnumerable<object[]> Cases
+            => _privateCases ?? (_privateCases =
+                   InsertDimension(GetBaseCases(), AllWhiteSpaceAndCommentOptions).ToArray()
+               );
+    }
+}
---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/EnumWithEnumFieldsWithWhiteSpaceTestCases.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Collections;
+    using static Domain;
+    using static Identification;
+
+    internal class EnumWithEnumFieldsWithWhiteSpaceTestCases : TestCasesBase
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        private static IEnumerable<object[]> GetBaseCases()
+        {
+            yield return GetRange<object>(
+                GetIdent(3) // EnumName
+                , GetIdents(GetRange(1, 3)).ToArray() // FieldNames
+            ).ToArray();
+        }
+
+        protected override IEnumerable<object[]> Cases
+            => _privateCases ?? (_privateCases
+                   = InsertDimension(GetBaseCases(), AllWhiteSpaceAndCommentOptions).ToArray()
+               );
+    }
+}
