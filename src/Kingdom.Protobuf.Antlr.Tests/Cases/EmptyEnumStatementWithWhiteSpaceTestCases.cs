b6dc469db0afa69dd1447760cc6c413acc024720
Michael W Powell <mwpowellhtx@gmail.com>
2019-03-24 21:26:31 -0400

    added white space test cases extending from the existing ones

---
+++ src/Kingdom.Protobuf.Antlr.Tests/Cases/EmptyEnumStatementWithWhiteSpaceTestCases.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once IdentifierTypo
+namespace Kingdom.Protobuf
+{
+    using static Domain;
+
+    internal class EmptyEnumStatementWithWhiteSpaceTestCases : EmptyEnumStatementTestCases
+    {
+        private static IEnumerable<object[]> _privateCases;
+
+        protected override IEnumerable<object[]> Cases
+            => _privateCases ?? (_privateCases
+                   = InsertDimension(base.Cases, AllWhiteSpaceAndCommentOptions).ToArray()
+               );
+    }
+}
