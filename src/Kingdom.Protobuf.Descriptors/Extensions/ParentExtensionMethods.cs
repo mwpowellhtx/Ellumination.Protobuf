59b0a4b7cb38c0079161eab0c687201455df884d
Michael W Powell <mwpowellhtx@gmail.com>
2019-04-15 16:26:36 -0400

    refactored parent extension methods a little bit

--- src/Kingdom.Protobuf.Descriptors/Extensions/ParentExtensionMethods.cs
+++ src/Kingdom.Protobuf.Descriptors/Extensions/ParentExtensionMethods.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // ReSharper disable once IdentifierTypo
 namespace Kingdom.Protobuf
 {
-    internal static class ParentExtensionMethods
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ParentExtensionMethods
     {
+        /// <summary>
+        /// Returns the <paramref name="descriptor"/> Parentage starting with the parent-most
+        /// <see cref="IDescriptor"/> in the ownership chain.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="descriptor"></param>
+        /// <returns></returns>
+        public static IEnumerable<IDescriptor> GetParentage<T>(this T descriptor)
+            where T : class, IDescriptor
+        {
+            if (!(descriptor.Parent is null) || descriptor.Parent is IDescriptor)
+            {
+                foreach (var parent in descriptor.GetParentage())
+                {
+                    yield return parent;
+                }
+            }
+
+            yield return descriptor;
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -14,7 +39,7 @@ namespace Kingdom.Protobuf
         /// <param name="item"></param>
         /// <param name="value"></param>
         /// <param name="replaceParent"></param>
-        public static void ReplaceItemParent<TItem, T>(this T parent, ref TItem item
+        internal static void ReplaceItemParent<TItem, T>(this T parent, ref TItem item
             , TItem value, Action<TItem, T> replaceParent)
             where T : IParentItem
         {
